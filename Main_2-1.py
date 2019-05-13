a5dda5b1513c1d7043ffc9e61fea9783b16769c4
nigglir <nigglir@student.ethz.ch>
2019-05-13 17:37:49 +0200

    Local tt-split

--- Main_2-1.py
+++ Main_2-1.py
@@ -1,14 +1,10 @@
 import numpy as np
-import os
-import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout
 import pandas as pd
 import keras
 from keras.utils import np_utils
-from sklearn.decomposition import KernelPCA, PCA, TruncatedSVD
-from sklearn.utils.class_weight import compute_class_weight, compute_sample_weight
+from sklearn.decomposition import PCA
 
 train = pd.read_hdf("train.h5", "train")
 test = pd.read_hdf("test.h5", "test")
@@ -28,11 +24,13 @@ y = np_utils.to_categorical(y)
 
 allData = np.vstack((X, test))
 
-n_clusters = 100
+n_clusters = 80
 pca = PCA(n_components=n_clusters, copy=False)
 allData = pca.fit_transform(X=allData)
 X = pca.transform(X=X)
 test = pca.transform(X=test)
+
+X_train, X_test, y_train, y_test = (train_test_split(X, y, test_size=0.33, random_state=11312))
 
 batch_size = 250
 
@@ -48,12 +46,18 @@ model.add(Dense(5, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='Nadam', metrics=['accuracy'])
 
-model.fit(X, y, batch_size=batch_size, epochs=2000, verbose=1)
+# model.fit(X, y, batch_size=batch_size, epochs=2000, verbose=1)
+model.fit(X_test, y_test, batch_size=batch_size, epochs=2000, verbose=1)
+
+perf = model.evaluate(X_test, y_test)
+print('done')
+print(perf)
+exit()
 
 dataYPredict = model.predict(test)
 y_pred = np.argmax(dataYPredict, axis=1)
 
 resf = pd.DataFrame({'Id': index, 'y': y_pred})
-resf.to_csv('res.csv', index = False)
+resf.to_csv('res_2-1.csv', index = False)
 print('Done')
 
